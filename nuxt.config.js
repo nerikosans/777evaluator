d506aac3f76c2c1582213991433c5e32c135130d
Ken Ogura <nekarugo628@gmail.com>
2020-10-04 16:25:51 +0900

    add nuxt.config.js

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+/* nuxt.config.js */
+// `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+        router: {
+          base: '/<repository-name>/'
+        }
+      }
+    : {}
+
+export default {
+  ...routerBase
+}
