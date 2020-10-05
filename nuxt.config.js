f9b8c3cc2c7b3e8681a5be284fd509bc9caa838f
Ken Ogura <nekarugo628@gmail.com>
2020-10-05 23:00:30 +0900

    ョ

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,5 +12,20 @@ const routerBase =
 export default {
     ssr:false,
     target: 'static',
+    head: {
+        title: '胡算器',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+          { hid: 'og:site_name', property: 'og:site_name', content: '胡算器' },
+          { hid: 'og:type', property: 'og:type', content: 'website' },
+          { hid: 'og:url', property: 'og:url', content: 'https://catupper.github.io/777evaluator/' },
+          { hid: 'og:title', property: 'og:title', content: '胡算器' },
+          { hid: 'og:description', property: 'og:description', content: 'b n ワ。' },
+          { hid: 'og:image', property: 'og:image', content: '/img/seven.jpg' },
+          { hid: 'twitter:card', content: 'summary'},
+        ]
+      },
     ...routerBase
 }
